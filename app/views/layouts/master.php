8be51336fa4f1e3af205df98cf6678bf39688df6
Richard Mohammed <richmoh@yahoo.co.uk>
2013-08-08 14:44:31 +0100

    Bootstrap installed. I love it when a plan comes together

--- app/views/layouts/master.php
+++ app/views/layouts/master.php
@@ -13,8 +13,11 @@
         <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
 
         <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+        <link rel="stylesheet" href="bootstrap/css/bootstrap-reponsive.css">
         <link rel="stylesheet" href="css/main.css">
         <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+        <script src="bootstrap/js/bootstrap.js"></script>
     </head>
     <body>
         <!--[if lt IE 7]>
